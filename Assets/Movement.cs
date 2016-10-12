69495dd4fa2287a1f8b9692beb5aad8b81c95e75
Tim Lindstam <tim.sunds@aol.com>
2016-10-12 11:45:35 +0200

    Init commit

---
+++ Assets/Movement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+    public Vector3 Speed;
+    private Rigidbody rigidbody;
+    //public Vector2 speedy;
+    // Use this for initialization
+    void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rigidbody.AddForce(Speed * Time.deltaTime, ForceMode.VelocityChange);
+        }
+	
+	}
+}
